c415184d63deb9f9feb765f4d4003ac000eb3998
kartikpavan <kartik.varanasi@yahoo.com>
2022-08-08 18:17:35 +0530

    ss folder and UI improvement

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { userSelector } from "../../features/auth.js";
-import { GrLogout } from "react-icons/gr";
-import { Link } from "react-router-dom";
-import { useGetListQuery } from "../../services/TMDB.js";
-import { Movie } from "../index.js";
+import React, {useEffect} from "react";
+import {useSelector} from "react-redux";
+import {userSelector} from "../../features/auth.js";
+import {GrLogout} from "react-icons/gr";
+import {Link} from "react-router-dom";
+import {useGetListQuery} from "../../services/TMDB.js";
+import {Movie} from "../index.js";
 
 function Profile() {
-  const { isAuthenticated, user } = useSelector(userSelector);
+  const {isAuthenticated, user} = useSelector(userSelector);
   const logOut = () => {
     localStorage.clear();
     window.location.href = "/";
   };
-  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({
+  const {data: favoriteMovies, refetch: refetchFavorites} = useGetListQuery({
     listName: "favorite/movies",
     session_id: localStorage.getItem("session_id"),
     accountId: user.id,
     page: 1,
   });
-  const { data: watchlistMovies, refetch: refetchWatchlist } = useGetListQuery({
+  const {data: watchlistMovies, refetch: refetchWatchlist} = useGetListQuery({
     listName: "watchlist/movies",
     session_id: localStorage.getItem("session_id"),
     accountId: user.id,
@@ -33,13 +33,13 @@ function Profile() {
   return (
     <section className="w-full h-full p-6">
       <div className="flex justify-between items-center py-4">
-        <h1 className="text-4xl "> My Profile </h1>
-        <h3 className="text-2xl text-info-content border border-info-content p-2">
-          {" "}
+
+        <h3 className="text-md md:text-2xl text-info-content border border-info-content p-2">
+
           {user.username}
         </h3>
         <button className="btn btn-accent flex gap-4" onClick={logOut}>
-          LogOut <GrLogout size={22} color={"white"} />
+          LogOut <GrLogout size={22} color={"white"}/>
         </button>
       </div>
       <main>
@@ -57,13 +57,13 @@ function Profile() {
             <h1 className="text-3xl font-semibold">Favorite Movies</h1>
             <div className="flex flex-wrap pt-2 pb-6 gap-6">
               {favoriteMovies?.results.map((item) => {
-                return <Movie key={item.id} movie={item} />;
+                return <Movie key={item.id} movie={item}/>;
               })}
             </div>
             <h1 className="text-3xl font-semibold">Watchlist Movies</h1>
             <div className="flex flex-wrap pt-2 pb-6 gap-6">
               {watchlistMovies?.results.map((item) => {
-                return <Movie key={item.id} movie={item} />;
+                return <Movie key={item.id} movie={item}/>;
               })}
             </div>
           </div>
