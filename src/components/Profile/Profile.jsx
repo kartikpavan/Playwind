2c7ad5319cc1fc7bfb1520dd3834bc5a2ce2579b
kartikpavan <kartik.varanasi@yahoo.com>
2022-08-08 15:26:40 +0530

    user Profile set up

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {userSelector} from "../../features/auth.js";
+import {GrLogout} from 'react-icons/gr'
+import {Link} from "react-router-dom";
 
 function Profile() {
-  const {id} = useParams()
-  const {isAuthenticated, user} = useSelector(userSelector)
-  console.log(user.username)
-  return (<section>
-    <div>
-      <h1>profile userName - {user?.username}</h1>
-      <h2>profile Id - {id}</h2>
-    </div>
-  </section>);
+  const {isAuthenticated, user} = useSelector(userSelector);
+  const logOut = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  }
+  const isFavorite = [];
+  return (
+    <section className="w-full h-full p-6">
+      <div className="flex justify-between items-center py-4">
+        <h1 className='text-4xl '> My Profile </h1>
+        <button className="btn btn-accent flex gap-4" onClick={logOut}>LogOut <GrLogout size={22} color={"white"}/>
+        </button>
+      </div>
+      <main>
+        {!isFavorite.length ? (
+            <p className="text-xl italic underline underline-offset-4">Add Favorites or watchlist some movies them to see
+              them here <Link to={"/"} className="text-2xl text-red-400">Browse Now...</Link></p>) :
+
+          <p>Favorite Movies</p>}
+      </main>
+    </section>);
 }
 
 export default Profile;
